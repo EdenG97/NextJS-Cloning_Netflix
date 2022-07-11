5f75250ef211863b44abf3203991f38b77ac0a60
Eden <edenginting90@yahoo.com>
2022-07-11 12:23:43 +0800

    moved the useSession from navigation to layout

--- components/header/Layout.tsx
+++ components/header/Layout.tsx
@@ -2,18 +2,20 @@ import NotificationContext from "../../store/NotificationContext";
 import Notification from "../ui/Notification";
 import Navigation from "./Navigation";
 import React, { Fragment, useContext } from "react";
+import { useSession } from "next-auth/react";
 
 type Children = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: Children): JSX.Element => {
+  const { status } = useSession();
   const notifContext = useContext(NotificationContext);
 
   return (
     <Fragment>
       <Notification status={notifContext.notification} />
-      <Navigation />
+      {status === "unauthenticated" && <Navigation />}
       <main>{children}</main>
     </Fragment>
   );
--- components/header/Navigation.tsx
+++ components/header/Navigation.tsx
@@ -2,12 +2,8 @@ import Link from "next/link";
 import Image from "next/image";
 import classes from "./Navigation.module.css";
 import { useRouter } from "next/router";
-import { useSession } from "next-auth/react";
-import { Fragment } from "react";
 
 const Navigation = (): JSX.Element => {
-  const { status } = useSession();
-
   const router = useRouter();
   const path = router.pathname;
 
@@ -17,35 +13,31 @@ const Navigation = (): JSX.Element => {
     : (border = classes["navbar-container"]);
 
   return (
-    <Fragment>
-      {status === "unauthenticated" && (
-        <header className={border}>
-          <ul className={classes["navbar-list"]}>
-            <li className={classes.image}>
-              <Link href="/" passHref>
-                <a>
-                  <Image
-                    src="/netflix.svg"
-                    alt="Netflix"
-                    width={160}
-                    height={100}
-                  />
-                </a>
-              </Link>
-            </li>
-            {(path === "/" || path === "/signup") && (
-              <li className={classes.signin}>
-                <Link href="/login">
-                  <a>
-                    <button>Sign In</button>
-                  </a>
-                </Link>
-              </li>
-            )}
-          </ul>
-        </header>
-      )}
-    </Fragment>
+    <header className={border}>
+      <ul className={classes["navbar-list"]}>
+        <li className={classes.image}>
+          <Link href="/" passHref>
+            <a>
+              <Image
+                src="/netflix.svg"
+                alt="Netflix"
+                width={160}
+                height={100}
+              />
+            </a>
+          </Link>
+        </li>
+        {(path === "/" || path === "/signup") && (
+          <li className={classes.signin}>
+            <Link href="/login">
+              <a>
+                <button>Sign In</button>
+              </a>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </header>
   );
 };
 
