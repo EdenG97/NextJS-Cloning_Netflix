142cdbd1546a4ac3d1b5fd79c0f7c00c3b8763d5
Eden <edenginting90@yahoo.com>
2022-07-12 14:12:17 +0800

    added note in the browse page

--- components/browse-page/Main.tsx
+++ components/browse-page/Main.tsx
@@ -1,8 +1,17 @@
+import { Fragment } from "react";
 import classes from "./Main.module.css";
 
 const Main = (): JSX.Element => {
   return (
-    <h1 className={classes.style}>Still On Development</h1>
+    <div className={classes.style}>
+      <h1>Still In Development</h1>
+      <p>
+        Because this page set cookies for authenticated
+        user, you have to clear cookies in the{" "}
+        <span>DevTools</span> to see the header on landing
+        page
+      </p>
+    </div>
   );
 };
 
